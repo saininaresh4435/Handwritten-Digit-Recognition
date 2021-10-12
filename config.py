7c035546b4e2322e73f60051103f5b2a191e9783
naresh <saininaresh4435@gmail.com>
2021-10-12 16:11:41 +0530

    handwritten digit recognition repo

---
+++ config.py
@@ -0,0 +1,10 @@
+
+SAVE_MODEL=True
+SAVE_MODEL_PATH="/home/naresh/Tarento/Build_Project/gujrat_sever/hw_recognition_api/models/trained_resnet_model_v3.h5"
+EPOCH=50
+BATCH_SIZE=64
+PRETRAINED_WEIGHT_PATH='/home/naresh/Tarento/Build_Project/gujrat_sever/trained_resnet_model_v2_10.h5'
+IMAGE_PATH="/home/naresh/Tarento/Build_Project/gujrat_sever/server_training_gujrat_handwritten/preprocess_data/*/*/*.jpg"
+TEST_DATA_SIZE=16000
+FINE_TUNE=True
+DATA_AUGMENTATION=False
